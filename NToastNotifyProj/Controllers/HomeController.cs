628917dd45f390a0180f67879da82a3a3ff0fd30
Chandra Shakar <Chandra.Shakar@bd.crystal-martin.com>
2020-06-28 00:21:37 +0600

    Add project files.

---
+++ NToastNotifyProj/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using NToastNotify;
+
+using NToastNotifyProj.Models;
+
+namespace NToastNotifyProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IToastNotification toastNotification;
+
+        public HomeController (ILogger<HomeController> logger, IToastNotification toastNotification)
+        {
+            _logger = logger;
+            this.toastNotification = toastNotification;
+        }
+
+        public IActionResult Index ()
+        {
+            toastNotification.AddSuccessToastMessage("Notification from NToastNotify"); // it will be load when home page rendered
+            return View();
+        }
+
+        public IActionResult Privacy ()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error ()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NToastNotifyProj/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NToastNotifyProj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
